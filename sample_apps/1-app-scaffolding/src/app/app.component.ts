aa5297c271942208340835bef486c17981811a9a
Jacob Wasilkowski <jwasilkowski@esri.com>
2018-02-26 11:22:02 -0600

    sample app 1 intialialized

--- sample_apps/1-app-scaffolding/src/app/app.component.ts
+++ sample_apps/1-app-scaffolding/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ArcGIS API for JavaScript with Angular: App Scaffolding';
+  title = 'app';
 }
